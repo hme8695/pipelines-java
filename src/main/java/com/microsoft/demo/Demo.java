f12a33f0d6a2a33eb4deb928ef1d2a3c9fdc798e
Rao Mothe <rao.mothe@parallon.com>
2020-09-29 13:41:55 -0500

    Testing for pipeline build

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,12 +1,14 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
-        if(flag){
-            System.out.println("I am covered");
-            return;
-        }
+	public void DoSomething(boolean flag) {
+		if (flag) {
+			System.out.println("I am covered");
+			return;
+		}
 
-        System.out.println("I am not covered");
-    }
+		System.out.println("I am not covered");
+		System.out.println("Code Checked to test the pipeline");
+
+	}
 }
